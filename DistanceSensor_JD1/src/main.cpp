13152aa86b8d191cf6d5b762b521abd27ff2ace8
Perchicoree <82548352+Perchicoree@users.noreply.github.com>
2025-02-26 13:52:53 -0800

    Update main.cpp, main funct. first draft, untested
    should now show distance in serial

--- DistanceSensor_JD1/src/main.cpp
+++ DistanceSensor_JD1/src/main.cpp
@@ -4,11 +4,25 @@
 
 
 void setup() {
+  int sda = 22;
+  int scl = 21;
   // put your setup code here, to run once:
   Serial.begin(115200);
-  Wire.begin(22, 21);
+  Wire.begin(sda, scl);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+
+  VL53L0X_RangingMeasurementData_t measurements;
+
+  if (measurements.RangeStatus == 0) {
+    Serial.print("Distance: ");
+    Serial.print(measurements.RangeMilliMeter);
+    Serial.print(" mm");
+  }
+  else
+    Serial.print("Out of range.");
+  
+  
+  delay (500);
 }
