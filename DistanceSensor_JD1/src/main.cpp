0a4b35518174c9a0212cb7a53c1c7874e8433689
Dario <mejiagad@oregonstate.edu>
2025-03-08 19:56:19 -0800

    Semi-Working

--- DistanceSensor_JD1/src/main.cpp
+++ DistanceSensor_JD1/src/main.cpp
@@ -1,18 +1,21 @@
 #include <Arduino.h>
-#include <WebServer.h>
+//#include <WebServer.h>
 #include <Wire.h>
 #include <Adafruit_VL53L0X.h>
 #include <LiquidCrystal_I2C.h> // If using I2C LC
+#include <string>
 
 
 // VL53L0X Configuration
 #define VL53L0X_XSHUT_PIN 5  // ESP32 pin connected to VL53L0X XSHUT
-#define VL53L0X_GPIO1_PIN 7  // ESP32 pin connected to VL53L0X GPIO1 (optional)
+//#define VL53L0X_GPIO1_PIN 7  // ESP32 pin connected to VL53L0X GPIO1 (optional)
 
 Adafruit_VL53L0X sensor = Adafruit_VL53L0X();
 
+
+
 // LCD Configuration (I2C)
-#define LCD_ADDRESS 0x27 // Check your LCD's I2C address (0x27 or 0x3F)
+#define LCD_ADDRESS 0x27 //Test Using 
 #define LCD_COLUMNS 16
 #define LCD_ROWS 2
 LiquidCrystal_I2C lcd(LCD_ADDRESS, LCD_COLUMNS, LCD_ROWS); //I2C LCD
@@ -34,7 +37,9 @@ void setup() {
   lcd.init();
   lcd.backlight();
   lcd.clear();
-  lcd.print("Initializing..."); // Display a message on the first row";
+  lcd.println("Initializing... "); // Display a message on the first row";
+  delay(1000);
+  lcd.clear();
 
   // VL53L0X Initialization
   
@@ -72,20 +77,64 @@ void loop() {
 uint16_t distance = sensor.readRange();
 
 
-if (measure.RangeStatus == 0) {
+if (distance <= 1200 || distance >= 150) {
   // Valid measurement
   Serial.print("Distance: ");
   Serial.print(measure.RangeMilliMeter);
   Serial.println(" mm");
+  lcd.setCursor(0, 0);
+  lcd.println("Distance: ");
   
-  lcd.setCursor(0, 1);
-  lcd.print(measure.RangeMilliMeter);
-  lcd.print(" mm        ");
+  lcd.setCursor(0,10);
+  //lcd.print(measure.RangeMilliMeter);
+  
+  lcd.print(distance);
+  lcd.print("    mm");
 } else {
-  Serial.println("Out of range");
+  Serial.println("OUT OF RANGE   ");
   lcd.setCursor(0, 1);
-  lcd.print("Out of range   ");
+  lcd.println("OUT OF RANGE    ");
 }
 
   delay(100); // Adjust delay as needed
 }
+
+
+// void setup() {
+//   Wire.begin();
+//   Serial.begin(115200);
+//   Serial.println("\nI2C Scanner");
+// }
+ 
+// void loop() {
+//   byte error, address;
+//   int nDevices;
+//   Serial.println("Scanning...");
+//   nDevices = 0;
+//   for(address = 1; address < 127; address++ ) {
+//     Wire.beginTransmission(address);
+//     error = Wire.endTransmission();
+//     if (error == 0) {
+//       Serial.print("I2C device found at address 0x");
+//       if (address<16) {
+//         Serial.print("0");
+//       }
+//       Serial.println(address,HEX);
+//       nDevices++;
+//     }
+//     else if (error==4) {
+//       Serial.print("Unknow error at address 0x");
+//       if (address<16) {
+//         Serial.print("0");
+//       }
+//       Serial.println(address,HEX);
+//     }    
+//   }
+//   if (nDevices == 0) {
+//     Serial.println("No I2C devices found\n");
+//   }
+//   else {
+//     Serial.println("done\n");
+//   }
+//   delay(5000);          
+// }
